4fe84f01c9e6de70d1cd6742bb20008dd57c1c6c
kanyaNgaboRichard <iirisi454@gmail.com>
2026-08-25 12:31:33 +0200

    Remove backend folder - now maintained in separate repo (digital-signature-platform-backend)

--- backend/src/routes/workflowRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createWorkflow } = require('../controllers/workflowController');
-const authenticateToken = require('../middleware/authMiddleware');
-
-// Protect the route with JWT middleware (Reading the HTTP-Only cookie)
-router.post('/create', authenticateToken, createWorkflow);
-
-module.exports = router;
